a16e83915a09cd5162e488f40f8a7540c810d8b4
chenhuayu-iiii <2312170@mail.nankai.edu.cn>
2025-10-20 19:58:21 +0800

    完成best_fit_pmm.c的best_fit_free_pages函数实现

--- lab2/kern/mm/best_fit_pmm.c
+++ lab2/kern/mm/best_fit_pmm.c
@@ -1,7 +1,8 @@
+#include <best_fit_pmm.h>
 #include <pmm.h>
 #include <list.h>
 #include <string.h>
-#include <best_fit_pmm.h>
+
 #include <stdio.h>
 
 /* In the first fit algorithm, the allocator keeps a list of free blocks (known as the free list) and,
@@ -74,10 +75,8 @@ best_fit_init_memmap(struct Page *base, size_t n) {
         assert(PageReserved(p));
         /*LAB2 EXERCISE 2: YOUR CODE*/ 
         // 清空当前页框的标志和属性信息，并将页框的引用计数设置为0
-
-        p->flags = p->property = 0;
-        set_page_ref(p,0);
-
+        p->flags = 0;                    // 清空标志
+        set_page_ref(p, 0);  
 
     }
     base->property = n;
@@ -93,16 +92,13 @@ best_fit_init_memmap(struct Page *base, size_t n) {
             // 编写代码
             // 1、当base < page时，找到第一个大于base的页，将base插入到它前面，并退出循环
             // 2、当list_next(le) == &free_list时，若已经到达链表结尾，将base插入到链表尾部
-
-            if(base < page){
+            if (base < page) {
                 list_add_before(le, &(base->page_link));
                 break;
-            }
-            else if(list_next(le) == &free_list){
-
+            } else if (list_next(le) == &free_list) {
                 list_add(le, &(base->page_link));
+                break;
             }
-
 
         }
     }
@@ -124,9 +120,10 @@ best_fit_alloc_pages(size_t n) {
 
     while ((le = list_next(le)) != &free_list) {
         struct Page *p = le2page(le, page_link);
+        // 修改为最佳适应：寻找满足需求的最小空闲块
         if (p->property >= n && p->property < min_size) {
+            min_size = p->property;
             page = p;
-            min_size = p->property; // 更新最小空闲页框大小
         }
     }
 
@@ -157,13 +154,9 @@ best_fit_free_pages(struct Page *base, size_t n) {
     /*LAB2 EXERCISE 2: YOUR CODE*/ 
     // 编写代码
     // 具体来说就是设置当前页块的属性为释放的页块数、并将当前页块标记为已分配状态、最后增加nr_free的值
-
-    base->property = n;
-    SetPageProperty(base);
-    nr_free += n;
-
-
-
+    base->property = n;           // 设置释放的页块数
+    SetPageProperty(base);        // 标记为已分配状态
+    nr_free += n; 
 
     if (list_empty(&free_list)) {
         list_add(&free_list, &(base->page_link));
@@ -190,14 +183,12 @@ best_fit_free_pages(struct Page *base, size_t n) {
         // 3、清除当前页块的属性标记，表示不再是空闲页块
         // 4、从链表中删除当前页块
         // 5、将指针指向前一个空闲页块，以便继续检查合并后的连续空闲页块
-
-        if(p + p->property == base){
-            p->property += base->property;
-            ClearPageProperty(base);
-            list_del(&(base->page_link));
-            base = p;
+        if (p + p->property == base) {
+            p->property += base->property;    // 更新前一个空闲页块的大小
+            ClearPageProperty(base);          // 清除当前页块的属性标记
+            list_del(&(base->page_link));     // 从链表中删除当前页块
+            base = p;                         // 将指针指向前一个空闲页块
         }
-
     }
 
     le = list_next(&(base->page_link));
@@ -269,8 +260,6 @@ basic_check(void) {
 
 // LAB2: below code is used to check the best fit allocation algorithm (your EXERCISE 1) 
 // NOTICE: You SHOULD NOT CHANGE basic_check, default_check functions!
-
-
 static void
 best_fit_check(void) {
     int score = 0 ,sumscore = 6;
